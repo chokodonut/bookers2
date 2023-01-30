1948df47a1ef97a0e5b61ff6faa3b3568ba7f008
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-01-30 13:18:11 +0000

    チェックシート19まで

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,11 @@
+class BooksController < ApplicationController
+  def new
+    @books = Book.new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_29_054542) do
+ActiveRecord::Schema.define(version: 2023_01_30_100141) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2023_01_29_054542) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string "user_id"
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "homes", force: :cascade do |t|
     t.string "title"
     t.string "body"
@@ -54,6 +62,7 @@ ActiveRecord::Schema.define(version: 2023_01_29_054542) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
