1a89f6396809f5898e2b16ed434ae9372cc4d6cf
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-01-31 13:10:19 +0000

    booksページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,24 @@
 class BooksController < ApplicationController
-  def new
-    @books = Book.new
-  end
+  
   
   def create
+    @book = Book.new(book_params)
+    @book.save
+  end
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+    redirect_to books_path
   end
   
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
-resources :users, only: [:edit, :show]
-resources :books, only: [:new, :edit]
-resources :homes, only: [:new]
+resources :users, only: [:edit, :show, :update]
+resources :books, only: [:new, :edit, :create, :index, :update]
+get "/homes/about" => "homes#about", as: "about"
 end
