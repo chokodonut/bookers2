a41611161adf7163c5be5c71ef71605a06fe4f75
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-02-01 13:25:22 +0000

    user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,12 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    # @user = current_user
+    @books = @user.books
+    #controllerになくてもいい
+  end  
+  
+  def index
+    
   end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
-resources :users, only: [:edit, :show, :update]
-resources :books, only: [:new, :edit, :create, :index, :update]
+resources :users, only: [:edit, :show, :update, :index]
+resources :books, only: [:edit, :create, :index, :update, :show, :destroy]
 get "/homes/about" => "homes#about", as: "about"
 end
