97653044c29ab5fb3e958d06de0b06e07f23266d
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-02-02 13:28:48 +0000

    画像表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,32 @@
 class UsersController < ApplicationController
   def edit
+    # @user = User.new
+    # @user = User.find(params[:id])
+    @user = current_user
+
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
-  
+
   def show
     @user = User.find(params[:id])
     # @user = current_user
     @books = @user.books
     #controllerになくてもいい
-  end  
-  
+    # @user.user_id = current_user.id
+  end
+
   def index
-    
-  end  
+    @user_image = UserImage.all
+  end
+
+    private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
