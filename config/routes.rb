e277a8b670b6683deb8da7829ca0ae7af14ed20f
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-02-04 06:28:25 +0000

    header

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
-resources :users, only: [:edit, :show, :update, :index, :create]
+resources :users, only: [:edit, :show, :update, :index,]
 resources :books, only: [:edit, :create, :index, :update, :show, :destroy]
 get "/homes/about" => "homes#about", as: "about"
+post "/users/:id" => "books#show"
 end
